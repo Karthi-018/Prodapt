fa23e711095f2abe7a7109a98a5b7799d98bd7c3
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2023-07-26 20:18:34 +0530

    Add files via upload

---
+++ Lokesh.P/ClassWork/Operations.java
@@ -0,0 +1,32 @@
+public class Operations
+{
+    public static void main(String a[])
+    {
+        int num1 = Integer.parseInt(a[0]);
+        int num2 = Integer.parseInt(a[1]);
+        String opr = a[2];
+
+        switch(opr)
+        {
+
+            case "+" :
+                System.out.println("The Sum of Two numbers is " +(num1+num2));
+                break;
+            case "-" :
+                System.out.println("The Diff of Two numbers is " +(num1-num2));
+                break;
+            case "*" :
+                System.out.println("The Product of Two numbers is " +(num1*num2));
+                break;
+            case "/" :
+                System.out.println("The Div of Two numbers is " +(num1/num2));
+                break;
+            case "%" :
+                System.out.println("The Mod of Two numbers is " +(num1%num2));
+                break;
+            default :
+                System.out.println("Invalid inout");
+        }
+    }
+}{
+}
