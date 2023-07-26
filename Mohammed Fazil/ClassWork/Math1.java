68171fee50ab72984d2ef1f7f2308579a1a61485
MohammedFAZIL-H <140168392+MohammedFAZIL-H@users.noreply.github.com>
2023-07-26 19:44:04 +0530

    Add files via upload

---
+++ Mohammed Fazil/ClassWork/Math1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Math
+{
+public static void main(String[] args) {
+int x;
+int y;
+int z;
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter the side x: ");
+x= scanner.nextInt();
+System.out.println("Enter the side y: ");
+y= scanner.nextInt();
+z= (x*x + y*y);
+System.out.println(z);
+}
+}
