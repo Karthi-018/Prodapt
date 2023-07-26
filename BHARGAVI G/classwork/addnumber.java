72e4710564900558bae5e677325de5f4d05aafae
Bhargavi-g2001 <139844117+Bhargavi-g2001@users.noreply.github.com>
2023-07-26 22:19:18 +0530

    Add files via upload

---
+++ BHARGAVI G/classwork/addnumber.java
@@ -0,0 +1,42 @@
+class addnumber
+{
+	public static void main(String args[])
+{
+	int opr=Integer.parseInt(args[0]);
+	switch(opr)
+{
+	case 1:
+	System.out.println("1 2 3 4 5 6 7 8 9 10");
+	break;
+	case 2:
+	System.out.println("2 3 4 5 6 7 8 9 10 ");
+	break;
+	case 3:
+	System.out.println("3 4 5 6 7 8 9 10");
+	break;
+	case 4:
+	System.out.println("4 5 6 7 8 9 10");
+	break;
+	case 5:
+	System.out.println("5 6 7 8 9 10");
+	break;
+	case 6:
+	System.out.println("6 7 8 9 10");
+	break;
+	case 7:
+	System.out.println("7 8 9 10");
+	break;
+	case 8:
+	System.out.println("8 9 10");
+	break;
+	case 9:
+	System.out.println("9 10");
+	break;
+	case 10:
+	System.out.println("10");
+	break;
+
+}
+}
+}
+	
