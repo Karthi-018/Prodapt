1b860f1715a431776f12e32314f96f1a6be74584
ReshmaB27 <140167294+ReshmaB27@users.noreply.github.com>
2023-07-27 16:08:21 +0530

    Add files via upload

---
+++ Reshma B/Assignment/Overloading and Overriding/Overloading/Program 2/MainThingsAboutMe.java
@@ -0,0 +1,10 @@
+class MainThingsAboutMe
+{
+	public static void main(String args[])
+	{
+		MyName a=new MyName();
+		a.name("Nick", "Nelson");
+		a.name("Nicholas", "Darwin", "Nelson");
+		a.name("Nicky");
+	}
+}
---
+++ Reshma B/Assignment/Overloading and Overriding/Overloading/Program 2/MyName.java
@@ -0,0 +1,17 @@
+public class MyName
+{
+	
+	public static void name (String s1, String s2, String s3)
+	{
+		System.out.println("I was born "+s1+" "+s2+" "+s3+",");
+	}
+	public static void name (String s1, String s2)
+	{
+		System.out.println("My name is "+s1+" "+s2+".");
+	}
+	public static void name (String s1)
+	{
+		System.out.println("but people call me "+s1+".");
+	}
+
+}
