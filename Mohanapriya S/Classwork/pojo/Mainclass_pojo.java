2bee4ae1ceab1028cc5ecd5c29d52db98149508b
Mohanapriya S <140167284+Mohanapriya-Sankar@users.noreply.github.com>
2023-07-27 11:15:16 +0530

    Add files via upload

--- Mohanapriya S/Classwork/pojo/Mainclass_pojo.java
+++ Mohanapriya S/Classwork/pojo/Mainclass_pojo.java
@@ -1,29 +1,38 @@
 import java.util.*;
 class Mainclass_pojo
 {
-public static void main(String []args)
-{
-Scanner s=new Scanner(System.in);
-pojo a[]=new pojo[3];
-for (int i=0;i<3;i++)
-{
-int eid=s.nextInt();
-String Name=s.next();
-int eSalary=s.nextInt();
-pojo m=new pojo();
-m.seteid(eid);
-m.seteName(Name);
-m.seteSalary(eSalary);
-a[i]=m;
-}
-for(pojo g:a)
-{
-System.out.println(g);
-}
-}
-}
+   public static void main(String a[]);
+   Scanner s=new Scanner(System.in);
+   System.out.println("Enter the number of Employees " ) ;
+   int n=s.nextInt();
+   pojo arr[]=new pojo[n];
+   for(int i=0;i<n;i++)
+   {
+    pojo employ=new pojo();
+
+    System.out.println("Employee id: ");
+    int eid=s.nextInt();
+    employ.setdetails(eid);
+    System.out.println("Employee name: ");
+    String name=s.next();
+    employ.setdetails(eName);
+    System.out.println("Employee salary: ");
+    int eSalary=s.nextInt();
+    employ.setdetails(eSalary);
+
+    arr[i]=employ;
+
+   }
+   public void displaydetails(pojo...employ)
+   {
+    for(pojo p:employ)
+    {
+        System.out.println(p);
+    }
+   }
 
 
+}
 
 
 
