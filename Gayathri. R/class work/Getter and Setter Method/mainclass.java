51dfe8f26e823f7d5bf04a65eb11f3bd11b6b0ea
GayathriR80376 <140167544+GayathriR80376@users.noreply.github.com>
2023-07-27 20:36:59 +0530

    Add files via upload

---
+++ Gayathri. R/class work/Getter and Setter Method/mainclass.java
@@ -0,0 +1,11 @@
+class mainclass
+{
+	public static void main(String a[])
+	{
+		thisdemo e=new thisdemo();
+		e.setEid(101);
+		e.setEname("abc");
+		System.out.println(e.getEid());
+		System.out.println(e.getEname());
+}
+}
---
+++ Gayathri. R/class work/Getter and Setter Method/thisdemo.java
@@ -0,0 +1,21 @@
+class thisdemo
+{
+int eid;
+String ename;
+public void setEid(int eid)
+{
+this .eid=eid;
+}
+public int getEid()
+{
+return eid;
+}
+public void setEname(String ename)
+{
+this.ename=ename;
+}
+public String getEname()
+{
+return ename;
+}
+}
