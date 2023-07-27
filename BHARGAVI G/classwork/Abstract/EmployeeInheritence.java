8e2279d65b61a7c3a2e7b92d433fc237f5423d7c
Bhargavi-g2001 <139844117+Bhargavi-g2001@users.noreply.github.com>
2023-07-27 21:25:27 +0530

    Add files via upload

---
+++ BHARGAVI G/classwork/Abstract/EmployeeInheritence.java
@@ -0,0 +1,42 @@
+public class EmployeeInheritence
+{
+	int eid;
+	String eName;
+	long esalary;
+EmployeeInheritence(int eid,String eName,long esalary)
+{
+	this.eid=eid;
+	this.eName=eName;
+	this.esalary=esalary;
+}
+public void seteid(int eid)
+{
+	this.eid=eid;
+}
+public int geteid()
+{
+return eid;
+}
+public void seteName(String eName)
+{
+	this.eName=eName;
+}
+public String geteName()
+{
+return eName;
+}
+public void setesalary(long esalary)
+{
+	this.esalary=esalary;
+}
+public long getesalary()
+{
+return esalary;
+}
+}
+
+
+
+
+
+
