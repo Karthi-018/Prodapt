c3ddcda174d91948362b95062917bd7b57018bc0
poornesh-ck <140324102+poornesh-ck@users.noreply.github.com>
2023-07-28 09:01:38 +0530

    Add files via upload

---
+++ Poornesh C K/Assignments/ekart/searchType.java
@@ -0,0 +1,9 @@
+class searchType{
+	public void search(int productId){
+		System.out.println("watterbottle");
+	}
+	public void search(String productName){
+		System.out.println("watterbottle");
+	}
+
+}
