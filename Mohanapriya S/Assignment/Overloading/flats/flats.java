56e736bc0fba603ea80ef3186f69755210819eac
Mohanapriya S <140167284+Mohanapriya-Sankar@users.noreply.github.com>
2023-07-28 09:09:34 +0530

    Add files via upload

---
+++ Mohanapriya S/Assignment/Overloading/flats/flats.java
@@ -0,0 +1,20 @@
+class flats
+{
+String type=null;
+int rooms=0;
+int amount=0;
+flats(String type)
+{
+System.out.println(type+" | "+rooms+" rooms | Amount:"+amount);
+}
+flats(String type, int rooms)
+{
+System.out.println(type+" | "+ rooms+" rooms | Amount:"+amount);
+}
+flats(String type, int rooms, float amount)
+{
+System.out.println(type+" | "+rooms+" rooms | Amount:"+amount);
+}
+}
+
+
