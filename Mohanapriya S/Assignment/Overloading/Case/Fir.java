8baf0614f05286f59ee1ab9cfab58841a64cae72
Mohanapriya S <140167284+Mohanapriya-Sankar@users.noreply.github.com>
2023-07-28 09:07:28 +0530

    Add files via upload

---
+++ Mohanapriya S/Assignment/Overloading/Case/Fir.java
@@ -0,0 +1,23 @@
+class Fir
+{
+String name=null;
+int cases=0;
+float Imprisonment=0;
+String crime=null;
+Fir(String name)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+crime +" | Imprisonment: "+Imprisonment);
+}
+Fir(String name , int cases)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+crime +" | Imprisonment: "+Imprisonment);
+}
+Fir(String name , int cases, String crime)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+crime +" | Imprisonment: "+Imprisonment);
+}
+Fir(String name , int cases, String crime, float Imprisonment)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+crime +" | Imprisonment: "+Imprisonment);
+}
+}
