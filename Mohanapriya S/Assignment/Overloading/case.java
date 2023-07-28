a0673c47ea8f8180693c3af03edd4565c8a980c5
Mohanapriya S <140167284+Mohanapriya-Sankar@users.noreply.github.com>
2023-07-28 09:04:55 +0530

    Add files via upload

---
+++ Mohanapriya S/Assignment/Overloading/case.java
@@ -0,0 +1,23 @@
+class Fir
+{
+String name=null;
+int cases=0;
+float Imprisonment=0.0;
+String case=0;
+Fir(String name)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+case +" | Imprisonment: "+Imprisonment);
+}
+Fir(String name , int cases)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+case +" | Imprisonment: "+Imprisonment);
+}
+Fir(String name , int cases, String case)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+case +" | Imprisonment: "+Imprisonment);
+}
+Fir(String name , int cases, String case, float Imprisonment)
+{
+System.out.println("Accused name: "+name +" | No.of cases : "  +cases  +" | Case: "+case +" | Imprisonment: "+Imprisonment);
+}
+}
---
+++ Mohanapriya S/Assignment/Overloading/main_flats.java
@@ -0,0 +1,9 @@
+class main_flats
+{
+public static void main(String a[])
+{
+flats f1=new flats("Own House");
+flats f2=new flats("Rent House", 2);
+flats f3=new flats("Rent House", 3, 15000);
+}
+}
