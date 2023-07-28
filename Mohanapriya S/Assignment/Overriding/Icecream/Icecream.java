fe88ad5ff431a8c313211ec60ea08a08840538aa
Mohanapriya S <140167284+Mohanapriya-Sankar@users.noreply.github.com>
2023-07-28 12:34:46 +0530

    Add files via upload

---
+++ Mohanapriya S/Assignment/Overriding/Icecream/Icecream.java
@@ -0,0 +1,43 @@
+class Icecream
+{
+String colour;
+double grams;
+public void flavour(String colour,double grams)
+{
+}
+}
+
+class Ice1 extends Icecream
+{
+public void flavour(String colour,double grams)
+{
+System.out.println("Flavour of " +colour +"is Strawberry" +"Grams: "+grams);
+}
+}
+
+class Ice2 extends Icecream
+{
+public void flavour(String colour,double grams)
+{
+System.out.println("Flavour of " +colour +"is pista" +"Grams: "+grams);
+}
+}
+
+class Ice3 extends Icecream
+{
+public void flavour(String colour,double grams)
+{
+System.out.println("Flavour of " +colour +"is Vannils" +"Grams: "+grams);
+}
+}
+
+class Ice4 extends Icecream
+{
+public void flavour(String colour,double grams)
+{
+System.out.println("Flavour of " +colour +"is Chocolate" +"Grams: "+grams);
+}
+}
+
+
+
---
+++ Mohanapriya S/Assignment/Overriding/Icecream/Icecream_main.java
@@ -0,0 +1,14 @@
+class Icecream_main
+{
+public static void main(String a[])
+{
+Icecream r1=new Ice1();
+r1.flavour("Pink",50);
+Icecream r2=new Ice2();
+r2.flavour("Green",25.8);
+Icecream r3=new Ice3();
+r3.flavour("White",100);
+Icecream r4=new Ice4();
+r4.flavour("Brown",150.5);
+}
+}
