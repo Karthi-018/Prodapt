f011ed887a563398a1373fd1956049d4de407434
Yamini-M23 <140368248+Yamini-M23@users.noreply.github.com>
2023-07-28 22:03:14 +0530

    Add files via upload

---
+++ YAMINI M/ASSIGNMENTS/OVERRIDING/AUTOMOBILE/Automobile.java
@@ -0,0 +1,32 @@
+class Automobile
+{
+public void vehicle()
+{
+System.out.println("Automobile Car Models");
+}
+}
+class ManualCar extends Automobile
+{
+public void vehicle()
+{
+System.out.println("Manual car");
+}}
+class AutomaticCar extends Automobile
+{
+public void vehicle()
+{
+System.out.println("Automatic car");
+}}
+class BatteryCar extends Automobile
+{
+public void vehicle()
+{
+System.out.println("Battery car");
+}}
+class ElectricCar extends Automobile
+{
+public void vehicle()
+{
+System.out.println("Electric car");
+}}
+
---
+++ YAMINI M/ASSIGNMENTS/OVERRIDING/AUTOMOBILE/MainVehicle.java
@@ -0,0 +1,16 @@
+class MainVehicle
+{
+public static void main(String args[])
+{
+Automobile a=new Automobile();
+a.vehicle();
+ManualCar m= new ManualCar();
+m.vehicle();
+AutomaticCar a1=new AutomaticCar();
+a1.vehicle();
+BatteryCar b=new BatteryCar();
+b.vehicle();
+ElectricCar e=new ElectricCar();
+e.vehicle();
+}
+}
