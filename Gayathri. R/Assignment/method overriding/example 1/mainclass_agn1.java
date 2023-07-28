82256ab7f7ed58dae259ccd543fefa08f5fe2366
GayathriR80376 <140167544+GayathriR80376@users.noreply.github.com>
2023-07-28 10:44:15 +0530

    Add files via upload

---
+++ Gayathri. R/Assignment/method overriding/example 1/mainclass_agn1.java
@@ -0,0 +1,9 @@
+class mainclass_agn1
+{
+public static void main(String[] a)
+{
+
+buttonphone o=new smartphone();
+o.call();
+}
+}
---
+++ Gayathri. R/Assignment/method overriding/example 2/Acbus.java
@@ -0,0 +1,15 @@
+class Acbus extends MTCbus
+{
+public void ticket(int stop)
+{
+if(stop==1 || stop==2)
+{
+System.out.println("bus fare for ac bus is rs.15");
+}
+else
+{
+System.out.println("bus fare for ac bus is rs 30");
+}
+}
+
+}
