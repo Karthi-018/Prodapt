6e9436530b4b8b8231afa6d68ca3b554614f865e
Yamini-M23 <140368248+Yamini-M23@users.noreply.github.com>
2023-07-28 05:40:31 +0530

    Add files via upload

---
+++ YAMINI M/ASSIGNMENTS/OVERLOADING/Calculator.java
@@ -0,0 +1,22 @@
+class Calculator 
+{
+public void add(int n1,int n2)
+{
+System.out.println("The Sum of Two integers is:"+(n1+n2));
+}
+public int add(int n1,int n2)
+{
+return n1+n2;
+}
+
+
+public void add(double n1,double n2)
+{
+System.out.println("The Sum of Two double numbers is:"+(n1+n2));
+}
+public void add(String n1,String n2)
+{
+System.out.println("The Sum of Two String is:"+(n1+n2));
+}
+
+}
