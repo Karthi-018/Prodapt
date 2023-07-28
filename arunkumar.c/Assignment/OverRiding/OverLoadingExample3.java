9600e86f2db075bf75d13a0140d39eaeedba2d28
4RUN-30 <102476267+4RUN-30@users.noreply.github.com>
2023-07-28 11:10:08 +0530

    Add files via upload

---
+++ arunkumar.c/Assignment/OverRiding/OverLoadingExample3.java
@@ -0,0 +1,48 @@
+class Mobile{
+	void openApp(){
+		System.out.println("Opening mobile app");
+	}
+}
+
+class IOS extends Mobile{
+	void openApp(){
+		System.out.println("Opening IOS mobile app");
+	}
+}
+
+class Android extends Mobile{
+	void openApp(){
+		System.out.println("Opening ANDROID mobile app");
+	}
+}
+
+class RealMe extends Android{
+	void openApp(){
+		System.out.println("Opening Realme app");
+	}
+}
+
+class Redmi extends Android{
+	void openApp(){
+		System.out.println("Opening Redmi app");
+	}
+}
+
+class X extends IOS{
+	void openApp(){
+		System.out.println("Opening Iphone X app");
+	}
+}
+
+class XR extends IOS{
+	void openApp(){
+		System.out.println("Opening Iphone XR app");
+	}
+} 
+
+class OverLoadingExample3{
+	XR xr = new XR();
+	xr.openApp();
+	Redmi redmi = new Redmi();
+	redmi.openApp();
+}
