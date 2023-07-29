fc31ae19a7bfe67d4b6670f6f16450213081accb
GayathriR80376 <140167544+GayathriR80376@users.noreply.github.com>
2023-07-29 23:50:05 +0530

    Add files via upload

---
+++ Gayathri. R/Assignment 2/visiblearea.java
@@ -0,0 +1,14 @@
+class visiblearea
+{
+public static void main(String a[])
+{
+employeeAssignment access=new employeeAssignment();
+access.create();
+access.duplicate();
+access.duplicate();
+access.display();
+access.search();
+access.delete();
+access.display();
+}
+}
