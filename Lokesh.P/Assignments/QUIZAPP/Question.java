807982ca3dbcb273cb00f9b91390c98d4e74efd8
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2023-08-01 22:09:50 +0530

    Update Question.java

--- Lokesh.P/Assignments/QUIZAPP/Question.java
+++ Lokesh.P/Assignments/QUIZAPP/Question.java
@@ -7,27 +7,5 @@ public class Question {
         this.answer = answer;
     }
 
-    public String getQuestion() {
-        return question;
-    }
-
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-                "question='" + question + '\'' +
-                ", answer='" + answer + '\'' +
-                '}';
-    }
+    
 }
