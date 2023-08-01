e7614b1cdcb83ba83363b3ad9e1f4ca4bf0c9706
shifisj21 <140167033+shifisj21@users.noreply.github.com>
2023-08-01 16:00:52 +0530

    Exception handling

---
+++ Shifi S J/Classwork/Exceptiondemo.java
@@ -0,0 +1,37 @@
+public class Exceptiondemo
+{
+public static void main(String[] args)
+{
+String str ="HI";
+int result=0;
+int arr[]=new int[5];
+
+
+/*
+try{
+FileInputStream fis=new FileInputStream("myfile.txt");
+int c=0;
+while((c=fis.read())!!=-1)
+{
+System.out.println((char)c);
+}
+System.out.println();
+*/
+System.out.println("String length is "+str.length());
+result=10/2;
+for(int i=0;i<=5;i++)
+catch(ArithmeticException e)
+catch(Exception e)
+
+}
+finally{
+try{
+fis.close();
+}
+catch(Exception e)
+System.out.println(e)
+fis.close();
+}
+System.out.println("The Div result is "+result)
+System.out.println("The array element is ");
+for(int
