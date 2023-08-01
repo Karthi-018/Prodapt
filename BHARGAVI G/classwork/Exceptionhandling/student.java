389929d2061f7dfa7a2f7ae27bb993f637a471e3
Bhargavi-g2001 <139844117+Bhargavi-g2001@users.noreply.github.com>
2023-08-01 23:31:19 +0530

    Add files via upload

---
+++ BHARGAVI G/classwork/Exceptionhandling/student.java
@@ -0,0 +1,36 @@
+class student
+{
+int id;
+String name;
+int marks;
+student(int id,String name,int marks){
+this.id=id;
+this.name=name;
+this.marks=marks;
+}
+public void setid(int sid)
+{
+this.id=sid;
+}
+public int getid()
+{
+return id;
+}
+public void setname(String sname)
+{
+this.name=sname;
+}
+public String getname()
+{
+return name;
+}
+public void setmarks(int smarks)
+{
+this.marks=smarks;
+}
+public int getmarks()
+{
+return marks;
+}
+}
+
