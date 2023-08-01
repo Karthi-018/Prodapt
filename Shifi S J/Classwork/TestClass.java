7e1ccfb33327a8f7b00ef1780e99481df99052ec
shifisj21 <140167033+shifisj21@users.noreply.github.com>
2023-08-01 10:38:59 +0530

    Add files via upload

---
+++ Shifi S J/Classwork/TestClass.java
@@ -0,0 +1,25 @@
+interface IntefaceExample {
+
+	final int a = 10;
+
+	void display();
+}
+
+class TestClass implements In1 {
+
+
+	public void display(){
+	System.out.println("Shifi");
+	}
+
+	public static void main(String[] args)
+	{
+		TestClass t = new TestClass();
+		t.display();
+		System.out.println(a);
+	}
+}
+
+
+
+
