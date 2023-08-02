81695c987bf339f7f51b9d75b430e2b29d26943b
shifisj21 <140167033+shifisj21@users.noreply.github.com>
2023-08-02 10:29:32 +0530

    Add files via upload

---
+++ Shifi S J/Classwork/ExceptionHandling/exception.java
@@ -0,0 +1,23 @@
+public class exception {
+
+	public static void main(String[] args) {
+		
+		   try{  
+			    int a[]=new int[5];  
+			    a[5]=30/0;  
+			   }  
+			   catch(ArithmeticException e)
+		          {
+				   System.out.println("Arithmetic Exception occurs");
+				  }  
+			catch(ArrayIndexOutOfBoundsException e)
+		          {
+				   System.out.println("ArrayIndexOutOfBounds Exception occurs");
+				  }  
+			   catch(java.lang.Exception e)
+		          {
+				   System.out.println("Exception");
+				  }  		  
+			   System.out.println("Result");  
+	}
+}
