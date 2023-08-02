2c13adbe1ef44e67e7547c5cc87fba7e945cda9f
GayathriR80376 <140167544+GayathriR80376@users.noreply.github.com>
2023-08-02 10:14:16 +0530

    Add files via upload

---
+++ Gayathri. R/Assignment 2/EMS/Main_Emp.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+    public class Main_Emp {
+        static Scanner sc = new Scanner(System.in);
+        static Service employeeService = new Service();
+
+        static void userChoice()
+        {
+            System.out.println("Enter your choice \n1. Create New Employee\n2.Update Employee Details\n3.Search Employee Details \n4.Display All Employee Details\n 5.Delete Employee Detail\n 6.Exit");
+           
+            int choice = sc.nextInt();
+            switchChoice(choice);
+        }
+
+        static void switchChoice(int choice)
+        {
+
+            switch (choice)
+            {
+                case 1 :
+                    System.out.println("Creating Employee Details");
+                    System.out.println("Enter the Employee ID");
+                    int id = sc.nextInt();
+                    System.out.println("Enter the Employee Name");
+                    String name = sc.next();
+                    System.out.println("Enter the Employee Department ");
+                    String dept = sc.next();
+                    System.out.println("Enter the Employee Salary");
+                    double sal = sc.nextDouble();
+                    Employeemodel newEmpList[] = employeeService.create_emp(new Employeemodel(id,name,dept,sal));
+                    for (int i =0;i<newEmpList.length;i++)
+                        System.out.println(newEmpList[i]);
+                    userChoice(); break;
+                case 2 :
+                    System.out.println("Updating the Employee Details");
+                    int updateEid = sc.nextInt();
+
+                    Employeemodel updateEmp = employeeService.search_emp(updateEid);
+                    if(updateEmp != null)
+                    {
+                        System.out.println("Enter your Choice");
+                        System.out.println("1 -> Update Name");
+                        System.out.println("2-> Update Department");
+                        System.out.println("3 -> Update salary");
+                        int updateChoice = sc.nextInt();
+                        switch (updateChoice)
+                        {
+                            case 1 :
+                                System.out.println("Enter Employee Name to update");
+                                updateEmp.setName(sc.next());
+
+                                break;
+                            case 2 :
+                                System.out.println("Enter the Employee Department to update");
+                                updateEmp.setDept(sc.next());
+                                break;
+                            case 3 :
+                                System.out.println("Enter the Employee Salary to update ");
+                                updateEmp.setSal(sc.nextInt());
+                                break;
+                        }
+
+                        System.out.println(updateEmp);
+                    }
+                    userChoice();break;
+                case 3 :
+                    System.out.println("Searching Employee Details");
+                    System.out.println("Enter the Employee ID to Search");
+                    int eid = sc.nextInt();
+                    Employeemodel emp = employeeService.search_emp(eid);
+                    if(emp != null)
+                    {
+                        System.out.println(emp);
+                    }
+                    else {
+                        System.out.println("Employee Not Found");
+                    }
+                    userChoice();break;
+                case 4 :
+                    System.out.println("All Employee Details");
+                    Employeemodel empList[] = employeeService.employeeList();
+                    for(int i =0;i<empList.length;i++)
+                    {
+                        System.out.println(empList[i]);
+                    }
+                    userChoice();break;
+                case 5 :
+                    System.out.println("Deleting Employee Details");
+                    System.out.println("Enter employee id to delete");
+                    int eeid=sc.nextInt();
+
+                    employeeService.delete_emp(eeid);
+                    userChoice();break;
+                case 6 : System.exit(0);
+                default: System.out.println("Invalid Choice");System.exit(0);
+            }
+        }
+
+        public static void main(String[] args) {
+
+            employeeService.get_e_arr();
+            userChoice();
+
+
+        }
+    }
