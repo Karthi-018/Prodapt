5ba61ea213dfe271dbeffc5068bcfe2d778d2ccb
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2023-08-02 13:17:03 +0530

    Update Main.java

--- Lokesh.P/ClassWork/Collections/Main.java
+++ Lokesh.P/ClassWork/Collections/Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
        List data = new ArrayList();
+
        data.add(1);
        data.add(5);
        data.add(49);
@@ -9,5 +10,7 @@ public class Main {
        data.forEach(System.out::println);
        List<Integer> l =Arrays.asList(1,23,57,7);
         System.out.println(l);
+     System.out.println(l.size());
+        
     }
 }
