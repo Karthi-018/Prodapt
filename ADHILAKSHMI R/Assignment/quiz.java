6d57cf86aeffb08805c0bfdf350da74df9d66ae7
adhilakshmiragupathi <139843963+adhilakshmiragupathi@users.noreply.github.com>
2023-08-02 21:14:55 +0530

    Add files via upload

---
+++ ADHILAKSHMI R/Assignment/quiz.java
@@ -0,0 +1,144 @@
+import java.util.*;
+public class quiz{
+public static void main (String args[])
+{
+
+System.out.println("1.Number of primitive data types in Java are? \n a)6 \n b)7 \n c)8 \n d)9");
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your answer: ");
+int val=sc.nextInt();
+
+if(val==8){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("2. _____ is used to find and fix bugs in the Java programs. \n a)JVM \n b)JRE \n c)JDK \n d)JDB");
+
+System.out.println("Enter your answer: ");
+String S=sc.next();
+
+if(S.equals("JDB")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("3.Which of the following is a reserved keyword in Java?. \n a)object \n b)strictfp \n c)main \n d)system");
+
+System.out.println("Enter your answer: ");
+String S1=sc.next();
+
+if(S1.equals("strictfp")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("4.Which keyword is used for accessing the features of a package? \n a)package \n b)import \n c)extends \n d)export");
+
+System.out.println("Enter your answer: ");
+String S2=sc.next();
+
+if(S2.equals("import")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("5.What is the initial quantity of the ArrayList list? \n a)5 \n b)10 \n c)0 \n d)100");
+
+System.out.println("Enter your answer: ");
+int m=sc.nextInt();
+
+if(m==10){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("6.Which keyword is used for accessing the features of a package? \n a)package \n b)import \n c)extends \n d)export");
+
+System.out.println("Enter your answer: ");
+String S0=sc.next();
+
+if(S0.equals("import")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("7.What is the original name of Java Programming language? \n a)J++ \n b)OAK \n c)C++ \n d)TEAK");
+
+System.out.println("Enter your answer: ");
+String S9=sc.next();
+
+if(S9.equals("OAK")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("8.What type of Java Programs can be run inside a Java supported Web Browser? \n a)Stand alone \n b)Struts \n c)Applets \n d)AWT");
+
+System.out.println("Enter your answer: ");
+String S7=sc.next();
+
+if(S7.equals("Applets")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("9.What is the file name extension of a Java source program? \n a).J \n b).Ja \n c).Java \n d).Jax");
+
+System.out.println("Enter your answer: ");
+String S5=sc.next();
+
+if(S5.equals(".Java")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+
+
+System.out.println("10.Which among the following is not a valid Data Type in Java? \n a)int \n b)float \n c)bool \n d)long");
+
+System.out.println("Enter your answer: ");
+String S6=sc.next();
+
+if(S6.equals("bool")){
+System.out.println("Correct Answer");
+}
+else{
+System.out.println("Incorrect Answer");
+}
+}
+}
+
+
+
+
+
+
+
+
