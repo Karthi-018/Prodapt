04004fb37b0caf7e6414e05a552f13ad7d842230
Arunsajethe <140166903+Arunsajethe@users.noreply.github.com>
2023-08-03 22:38:04 +0530

    Add files via upload

---
+++ Arun Sajeth E/Assignment/Quiz using pojo class/Quiz_Service.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.util.*;
+public class Quiz_Service
+{
+    static Scanner sc= new Scanner(System.in);
+    static String[] answer=new String[10];
+    static int mark=0;
+    public static void quiz_working(Quiz[] quest)
+    {
+        int i=0;
+        while(i<10)
+        {
+            if(i==0)
+            {
+                System.out.println(quest[i]);
+                System.out.print("Enter the option: ");
+                answer[i]=sc.next();
+                i+=1;
+                continue;
+            }
+            System.out.print("\nEnter N for Next question and P for previous question: ");
+            String choice = sc.next();
+            if(i!=0 && choice.equals("N"))
+            {
+                System.out.println("\n"+quest[i]);
+                System.out.println("Enter the option: ");
+                answer[i]=sc.next();
+                i+=1;
+            }
+            else if(i!=9 && choice.equals("P"))
+            {
+                i-=2;
+                System.out.println("\n"+quest[i]);
+                System.out.println("Enter the option: ");
+                answer[i]=sc.next();
+                i+=2;
+            }
+            else
+            {
+                System.out.println("Invalid choice");
+            }
+
+        }
+    }
+
+    public static void display(Quiz[] quest)
+    {
+        for(int i=0;i<10;i++)
+        {
+            if(answer[i].equals(quest[i].getCrctAns()))
+            {
+                mark+=1;
+            }
+        }
+        System.out.println("\nYour score is "+mark+" out of 10.");
+    }
+}
