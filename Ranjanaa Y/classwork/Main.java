5afe80668a625583eaba4501acc01f0b6d909edf
RANJANAA20 <84500585+RANJANAA20@users.noreply.github.com>
2023-08-03 13:56:38 +0530

    Add files via upload

--- Ranjanaa Y/classwork/Main.java
+++ Ranjanaa Y/classwork/Main.java
@@ -1,61 +1,36 @@
 package org.example;
-import org.example.Emp.Student;
-
 import java.util.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //System.out.println();
-        Set<Student> data = new HashSet<>();
-        Main m = new Main();
-        Student s1 = new Student(1,"A",90);
-        Student s2 = new Student(2,"B",99);
-        Student s3 = new Student(3,"C",93);
-        Student s4 = new Student(4,"D",88);
-        Student s5 = new Student(5,"E",77);
+
+        ArrayList<PojoTelephone> at = new ArrayList<>();
+
+        PojoTelephone t1 = new PojoTelephone("Alia","9003291897");
+        PojoTelephone t2 = new PojoTelephone("Aysha","9003291897");
+        PojoTelephone t3 = new PojoTelephone("Aira","9003291897");
+        PojoTelephone t4 = new PojoTelephone("Aila","9003298979");
+        PojoTelephone t5 = new PojoTelephone("Arya","1234567890");
+
+        at.add(t1);
+        at.add(t2);
+        at.add(t3);
+        at.add(t4);
+        at.add(t5);
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name to get Phone no: ");
+        String uname = sc.next();
 
-        data.add(s1);
-        data.add(s2);
-        data.add(s3);
-        data.add(s4);
-        data.add(s5);
-        System.out.println(data);
-        ArrayList<Student> l = new ArrayList<>();
-        l.addAll(data);
-
-        System.out.println("Enter 1 to sort by name: ");
-        System.out.println("Enter 2 to sort by name: ");
-        int choice = sc.nextInt();
-        switch(choice)
+        Iterator<PojoTelephone> i = at.iterator();
+        while(i.hasNext())
         {
-            case 1 :
-                l.sort(new Comparator<Student>() {
-                    @Override
-                    public int compare(Student s1, Student s2) {
-                        if(s1.marks>s2.marks)
-                        {
-                            return -1;
-                        } else if (s1.marks< s2.marks)
-                        {
-                            return 1;
-                        }
-                        else {
-                            return 0;
-                        }
-                    }
-                });
-                System.out.println("The sorted list of mark is"+l);
-            case 2 : l.sort(new Comparator<Student>() {
-                @Override
-                public int compare(Student s1, Student s2) {
-
-                    return s1.name.compareTo(s2.name);
-                }
-            });
-                System.out.println("The sorted list of name is"+l);
+            PojoTelephone t = i.next();
+            if(uname.equals(t.getName()))
+            {
+                System.out.println(t);
+            }
         }
     }
 }
