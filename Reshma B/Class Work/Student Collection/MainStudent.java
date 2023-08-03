49e406a1d775bb8ad29286d6a99abb0015a21d7c
ReshmaB27 <140167294+ReshmaB27@users.noreply.github.com>
2023-08-03 09:57:33 +0530

    Update MainStudent.java

--- Reshma B/Class Work/Student Collection/MainStudent.java
+++ Reshma B/Class Work/Student Collection/MainStudent.java
@@ -1,4 +1,68 @@
-package org.example.dao;
+package org.example;
+import org.example.dao.Student;
 
-public class MainStudent {
+
+import java.util.*;
+
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static Set<Student> stuArr=new HashSet<Student>();
+    public static List <Student> RRR = new ArrayList<>();
+    public static void switchChoice(int choice)
+    {
+        switch(choice){
+            case 1:
+                RRR.sort(new Comparator<Student>() {
+                    public int compare(Student O1, Student O2) {
+                        return O1.getSname().compareTo(O2.getSname());
+                    }
+                });
+                RRR.forEach(System.out::println);
+                break;
+            case 2:
+                RRR.sort(new Comparator<Student>(){
+                    public int compare (Student O1, Student O2)
+                    {
+                        if (O1.getMarks()<O2.getMarks())
+                        {
+                            return 1;
+                        }
+                        else if(O1.getMarks()>O2.getMarks())
+                        {
+                            return -1;
+                        }
+                        else
+                        {
+                            return 0;
+                        }
+                    }
+                });
+                RRR.forEach(System.out::println);
+                break;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+
+        Scanner s = new Scanner(System.in);
+
+        for(int i=0;i<3;i++)
+        {
+            int sid=s.nextInt();
+            String Sname=s.next();
+            int marks=s.nextInt();
+
+            stuArr.add(new Student(sid,Sname,marks));
+
+        }
+        RRR.addAll(stuArr);
+        System.out.print("Enter the choice:");
+        int choice=s.nextInt();
+        switchChoice(choice);
+    }
 }
