be83bf950402f2a5d65910070bbbae7fa95d1170
Hariharan2701 <114206942+Hariharan2701@users.noreply.github.com>
2023-08-03 21:37:23 +0530

    Add files via upload

---
+++ Hariharan G/Classwork/IO Operation/IO_OperationsNew.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class IO_OperationsNew implements Serializable {
+
+    public static void main(String[] args) throws IOException {
+        File file=new File("Hari.txt");
+        file.createNewFile();
+        try( FileInputStream fis=new FileInputStream(file);
+             FileOutputStream fos= new FileOutputStream(file,true);
+             BufferedOutputStream bos=new BufferedOutputStream(fos);)
+        {
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter text to write in file:\n");
+            String s=sc.nextLine();
+            byte[] arr=s.getBytes();
+            bos.write(arr);
+            System.out.println("SUCCESS!!!");
+
+
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try(FileInputStream fis=new FileInputStream("Hari.txt");
+        BufferedInputStream bis=new BufferedInputStream(fis);)
+        {
+            Scanner sc=new Scanner(file);
+            while(sc.hasNext()){
+                System.out.println(sc.next());
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
