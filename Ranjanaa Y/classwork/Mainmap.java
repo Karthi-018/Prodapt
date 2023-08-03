7fa057364815fb314d92a1dc263e161bcae19044
RANJANAA20 <84500585+RANJANAA20@users.noreply.github.com>
2023-08-03 14:43:06 +0530

    Update and rename Main.java to Mainmap.java

--- Ranjanaa Y/classwork/Mainmap.java
+++ Ranjanaa Y/classwork/Mainmap.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class Mainmap {
     public static void main(String[] args)
     {
         Map<String,String> map = new HashMap<>();
