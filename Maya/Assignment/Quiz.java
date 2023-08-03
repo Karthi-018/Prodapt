78b39fc932f141204fdbde33edfb1710816a1cf2
maya-06 <140168019+maya-06@users.noreply.github.com>
2023-08-03 21:54:15 +0530

    Update Quiz.java

--- Maya/Assignment/Quiz.java
+++ Maya/Assignment/Quiz.java
@@ -1,49 +1,75 @@
 import java.util.Scanner;
 
-public class Quiz {
-      static String[] qns = {
-        "How many primitive data types are there in Java?",
-        "Which of the following options is a valid class identifier in Java?",
-        "Java permits a class to extend only one other class directly. This is called",
-        "The ______ keyword is used to call the constructor of the parent class",
-        "OOP allows a class to make use of another class. This kind of relationship is called _________"
+ class Question {
+    private String question;
+    private String choices;
+    private char answer;
+
+    public Question(String question, String choices, char answer) {
+        this.question = question;
+        this.choices = choices;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getChoices() {
+        return choices;
+    }
+
+    public char getAnswer() {
+        return answer;
+    }
+}
+
+
+ class Quiz {
+    static Question[] questions = {
+        new Question("How many primitive data types are there in Java?", "a) 5\nb) 7\nc) 6\nd) 8", 'd'),
+        new Question("Which of the following options is a valid class identifier in Java?", "a) creditcard\nb) creditCard\nc) CreditCard\nd) 1creditCard", 'c'),
+        new Question("Java permits a class to extend only one other class directly. This is called", "a) Hierarchical inheritance\nb) single inheritance\nc) multilevel\nd) hybrid", 'b'),
+        new Question("The ______ keyword is used to call the constructor of the parent class", "a) this\nb) Parent class name\nc) super\nd) parent", 'c'),
+        new Question("OOP allows a class to make use of another class. This kind of relationship is called _________", "a) Generalization\nb) Utilization\nc) Composition\nd) Instantiation", 'b')
     };
-
-     static String[] choices = {
-        "a) 5\nb) 7\nc) 6\nd) 8",
-        "a) creditcard\nb) creditCard\nc) CreditCard\nd) 1creditCard",
-        "a) Hierarchical inheritance\nb) single inheritance\nc) multilevel\nd) hybrid",
-        "a) this\nb) Parent class name\nc) super\nd) parent",
-        "a) Generalization\nb) Utilization\nc) Composition\nd) Instantiation"
-    };
-
-     static char[] answers = {'d', 'c', 'b', 'c', 'b'};
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int score = 0;
         char inputVal;
-	
 
         System.out.println("Choose the correct answer");
 
-        for (int i = 0; i < qns.length; i++) {
-            System.out.println((i + 1) + ". " + qns[i]);
-            System.out.println(choices[i]);
 
-            do {
+        for (int i = 0; i < questions.length; i++)
+	 {
+
+                System.out.println((i + 1) + ". " + questions[i].getQuestion());
+                System.out.println(questions[i].getChoices());
+
+            do
+	    {
                 inputVal = scanner.next().charAt(0);
-
             } while (inputVal < 'a' || inputVal > 'd');
 
-
-            if (inputVal == answers[i]) {
-                score++;
+            if (inputVal == questions[i].getAnswer()) 
+	    {
+                // score++;
             }
-        }
 
-        System.out.println("Your score: " + score);
+            System.out.println("Enter 'p' for previous and 'n' for next question");
+            char norp = scanner.next().charAt(0);
+
+                if (norp == 'p') 
+		{
+                    i = i - 2;
+                }
+          
+	}
+
+        //System.out.println("Your score: " + score);
+        System.out.println("Quiz finished");
     }
 }
 
