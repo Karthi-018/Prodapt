3ef74e8b3ed61eddf2b64076f88ddcd335b39827
divyaasri-ar <139858200+divyaasri-ar@users.noreply.github.com>
2023-08-03 22:32:46 +0530

    Add files via upload

---
+++ Divyaa Sri A R/ClassWork/Serialization/Deserialization.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class Deserialization {
+    public static void main(String[] args) {
+        try(FileInputStream fis= new FileInputStream("demo.txt");
+            ObjectInputStream ois= new ObjectInputStream(fis)){
+            System.out.println(ois.readObject());
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
