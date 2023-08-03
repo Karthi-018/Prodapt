4f365d81f569f5954b7ebfd1fc92c7379c75c1b6
adhilakshmiragupathi <139843963+adhilakshmiragupathi@users.noreply.github.com>
2023-08-03 22:06:20 +0530

    Add files via upload

---
+++ ADHILAKSHMI R/Classwork/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+public class Main{
+    public static void main(String args[]){
+        String Str1="Hello";
+        String Str2="Hi Hello";
+        String Str3="Hi! How are you?";
+        System.out.println(Str3.charAt(6));
+        System.out.println(Str3.concat(Str3));
+        System.out.println(Str1.length());
+        System.out.println(Str1.equals(Str3));
+    }
+}
