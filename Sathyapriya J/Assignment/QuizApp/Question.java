1a03b578f45426ca393928402b0e5072cb15cd92
SathyaPriya-02 <140167236+SathyaPriya-02@users.noreply.github.com>
2023-08-03 12:35:17 +0530

    Update Question.java

--- Sathyapriya J/Assignment/QuizApp/Question.java
+++ Sathyapriya J/Assignment/QuizApp/Question.java
@@ -1,8 +1,8 @@
-class Question 
+public class Question
 {
-    int questionNo;
-    String question;
-    String options [];
+    private int questionNo;
+    private String question;
+    private String options [];
     private int correctAns;
 
     Question(int questionNo,String question,String options [],int correctAns)
@@ -12,12 +12,37 @@ class Question
         this.options = options;
         this.correctAns = correctAns;
     }
+    public void setQno(int questionNo)
+    {
+        this.questionNo= questionNo;
+    }
+    public int getQno()
+    {
+        return questionNo;
+    }
+    public void setQ(String question)
+    {
+        this.question= question;
+    }
+    public String getQ()
+    {
+        return question;
+    }
+    public void setOptions(String options[])
+    {
+        this.options = options;
+    }
+    public String[] getOptions()
+    {
+        return options;
+    }
 
     public boolean answerCheck(int selectedAns)
     {
         if(correctAns == selectedAns)
         {
             return true;
+
         }
         else
         {
