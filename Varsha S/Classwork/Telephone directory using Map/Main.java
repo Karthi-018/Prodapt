d59c4e474aafef9393b34dc737a3ee1ce1f899f3
Varsha-0108 <140167235+Varsha-0108@users.noreply.github.com>
2023-08-03 11:56:42 +0530

    Add files via upload

---
+++ Varsha S/Classwork/Telephone directory using Map/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> telephone = new TreeMap<>();
+        telephone.put("Varr", "6383266501");
+        telephone.put("Sneha", "9876543254");
+        telephone.put("Ragzz", "7654328796");
+        telephone.put("Sudha", "3421567890");
+        telephone.put("Hari", "123456790");
+        telephone.put("Sneha", "6573425679");
+
+        System.out.println(telephone.get("Sneha"));
+        System.out.println(telephone.keySet());
+        System.out.println(telephone.values());
+        System.out.println(telephone.entrySet());
+    }
+
+}
