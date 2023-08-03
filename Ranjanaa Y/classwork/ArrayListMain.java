1e4bcf1314734d50108ecff4aea60d4516dbea6f
RANJANAA20 <84500585+RANJANAA20@users.noreply.github.com>
2023-08-03 14:00:16 +0530

    Update and rename Main.java to ArrayListMain.java

--- Ranjanaa Y/classwork/ArrayListMain.java
+++ Ranjanaa Y/classwork/ArrayListMain.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class ArrayListMain {
     public static void main(String[] args) {
 
         ArrayList<PojoTelephone> at = new ArrayList<>();
