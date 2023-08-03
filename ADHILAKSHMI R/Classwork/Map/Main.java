39532cfed26198b6f558f21f3f5fb928d2dc9304
adhilakshmiragupathi <139843963+adhilakshmiragupathi@users.noreply.github.com>
2023-08-03 21:42:24 +0530

    Add files via upload

--- ADHILAKSHMI R/Classwork/Map/Main.java
+++ ADHILAKSHMI R/Classwork/Map/Main.java
@@ -1,48 +1,40 @@
-package org.example;
 
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
-    public static void main(String args[]) {
-        List<telephone> phonebook = new ArrayList<>();
-        Scanner sc = new Scanner(System.in);
-        int size=sc.nextInt();
-        for(int j=0;j<size;j++) {
-            String n = sc.next();
-            int b = sc.nextInt();
-            phonebook.add(new telephone(n,b));
-            System.out.println(n+" "+b);
+
+    public static void main(String[] args) {
+
+        Map<String, Long> phoneBook = new LinkedHashMap<String, Long>();
+
+        phoneBook.put("AAA", 9087654321l);
+        phoneBook.put("EEE", 9807654321l);
+        phoneBook.put("BBB", 9780765432l);
+        phoneBook.put("DDD", 9678054321l);
+        phoneBook.put("CCC", 1023456781l);
+        phoneBook.putIfAbsent("CCC", 12346789l);
+        phoneBook.remove("CCC");
+
+        System.out.println(phoneBook.entrySet());
+        System.out.println(phoneBook.get("AAA"));
+        System.out.println(phoneBook.keySet());
+        System.out.println(phoneBook.values());
+
+        for (String key : phoneBook.keySet()) {
+
+            System.out.println(key + " " + phoneBook.get(key));
         }
 
-        String searchName = "DDD";
-
-        int x = 0;
-        for (telephone t : phonebook) {
-
-            if (t.getName().equals(searchName)) {
-               // System.out.println("The number is " + t.getNumber());
-                x = 1;
-                System.out.println("The number is " + t.getNumber());
-            }
-        }
-            if(x==0){
-                System.out.println("The number is not found");
-            }
-
-
-
-        }
+        phoneBook.forEach((k, v) -> {
+            System.out.println(k + " " + v);
+        });
+    }
 }
-
-
-
-
-
-
-
-
-
