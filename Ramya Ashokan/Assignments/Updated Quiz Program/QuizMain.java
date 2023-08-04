e5e6cbb419af6b9dd01b771a8dcfb05fe5e151bd
Ramya-Ashokan <140167902+Ramya-Ashokan@users.noreply.github.com>
2023-08-04 15:34:46 +0530

    Add files via upload

--- Ramya Ashokan/Assignments/Updated Quiz Program/QuizMain.java
+++ Ramya Ashokan/Assignments/Updated Quiz Program/QuizMain.java
@@ -19,10 +19,12 @@ public class QuizMain {
         System.out.println(temp[0].getOpt3());
         System.out.println(temp[0].getOpt4());
         u_ans[0] = sc.next().charAt(0);
-        for(i=1;i<temp.length;i++) {
+        for(i=1;i<=temp.length;i++) {
+            if(i<=temp.length)
+            {
             System.out.println("Enter -1 ->Previous (or) 1->Next");
             choice=sc.nextInt();
-            sc.nextLine();
+            sc.nextLine();}
             if(i!=1&& choice==-1)
             {
                 i=i-2;
@@ -32,9 +34,14 @@ public class QuizMain {
                 System.out.println(temp[i].getOpt3());
                 System.out.println(temp[i].getOpt4());
                 u_ans[i] = sc.next().charAt(0);
+                if(i==temp.length+1)
+                {
+                    break;
+                }
                 i++;
+
             }
-            else if(i<10 && choice==1)
+            else if(i<10&& choice==1)
             {
                // i=i;
                 System.out.println(temp[i].getQuestion());
