191809f804bbf245d38e5ed499716bc795eef0bf
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2023-08-06 12:00:34 +0530

    Create Casting.java

---
+++ Lokesh.P/ClassWork/Casting.java
@@ -0,0 +1,26 @@
+class A
+{
+    public void show1()
+    {
+        System.out.println("A Show");
+    }
+}
+class B extends A
+{
+    public void show2()
+    {
+        System.out.println("B show");
+    }
+
+}
+public class Casting {
+    public static void main(String[] args) {
+        //upcasting
+        A obj = new B();
+        obj.show1();
+        //downcasting
+        B obj1=(B)obj;
+        obj1.show2();
+    }
+
+}
