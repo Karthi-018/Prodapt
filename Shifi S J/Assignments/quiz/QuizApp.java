44ae642c144a08f16340f80587793d143aad7347
shifisj21 <140167033+shifisj21@users.noreply.github.com>
2023-08-07 21:40:46 +0530

    Update QuizApp.java

--- Shifi S J/Assignments/quiz/QuizApp.java
+++ Shifi S J/Assignments/quiz/QuizApp.java
@@ -1,27 +1,30 @@
 import java.util.*;
-class QuizApp 
-{
+class QuizApp
+ {
     static QuizApp Question[] = new QuizApp[6];
     static int i=0;
 
     public static void main(String[] args)
-    {
+ {
         Scanner sc=new Scanner(System.in);
 
 
-        Question[0] = new quizPojo("1.The default value of a static integer variable of a class in Java is \n a)0 \n b)null \n c)garbage value \n d)1");
+        Question[0] = new quizPojo("4.Number of primitive data types in Java are ","b","\na.7","\nb.8","\nc.6","\nd.9");
 
-        Question[1] = new App("2.Which of the following options is a valid class identifier in Java ","c","\na.creditcard","\nb.creditCard","\nc.CreditCard","\nd.1creditCard");
+        Question[1] = new App("2.Which of the following options is a valid class identifier in Java","c","\na.creditcard","\nb.creditCard","\nc.CreditCard","\nd.1creditCard");
 
-        Question[2] = new quizPojo("3.The ______ keyword is used to call the constructor of the parent class ","c","\na.this","\nb.parent class name","\nc.super","\nd.parent");
+        Question[2] = new quizPojo("3.The ______ keyword is used to call the constructor of the parent class","c","\na.this","\nb.parent class name","\nc.super","\nd.parent");
 
-        Question[3] = new App("3.Which of the following variable declaration would NOT compile in a java program \n a)int var \n b)int VAR \n c)int var_1 \n d)int 1_var");
+        Question[3] = new App("3.Which of the following variable declaration would NOT compile in a java program","c","\na.int var","\nb.int VAR","\nc.int var_1","\nd.int 1_var");
 
-        Question[4] = new quizPojo("4.Which of the following is a member of the java.lang package \n a)Math \n b)List \n c)Queue \n d)Stack");
+        Question[4] = new quizPojo("4.Which of the following is a member of the java.lang package","a","\na.Math","\nb.List","\nc)Queue","\nd.Stack");
 
-        Question[5] = new quizPojo("5.What is the initial quantity of the ArrayList list? \n a)5 \n b)10 \n c)0 \n d)100");
+        Question[5] = new quizPojo("5.What is the initial quantity of the ArrayList list?","c","\na.5","\nb.10","\nc.0","\nd.100");
        
         Question[0].setOption4("\nd.10");//for updating incorrect option
+
+
+
 
         System.out.println("WELCOME TO MY QUIZ APP");
         
@@ -37,9 +40,10 @@ class QuizApp
             System.out.println("Enter Answer");
             String user_ans = sc.next();
 
-            System.out.println("1.NEXT QUESTION");
-            System.out.println("2.PREVIOUS QUESTION");
-            int choice = sc.nextInt();
+
+                System.out.println("1.NEXT QUESTION");
+                System.out.println("2.PREVIOUS QUESTION");
+                int choice = sc.nextInt();
 
                 switch (choice) {
                     case 1:
@@ -48,10 +52,10 @@ class QuizApp
                             if (user_ans.equals(Question[i].getAns())) {
                                 mark++;
                             }
+
                         }
                         i++;
                         break;
-                        
                     case 2:
                         if (i > 0 && i < Question.length-1) {
                            
@@ -61,11 +65,16 @@ class QuizApp
                         }
                         i--;
                         break;
-                }   
-        }
 
-        System.out.println("Thank you. Your score is :"  +mark+  "out of 10);
-    }
+                }
+
+
+            }
+
+        System.out.println("Thank you. Your score is :"  +mark+  "out of 10");
+
+
+           }
 }
 
 
@@ -75,9 +84,5 @@ class QuizApp
 
 
 
-
-
-
-    }
 
 
