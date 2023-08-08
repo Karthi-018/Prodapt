def8cb0b5071eb15a716225b97364ccbbcbd8a66
SathyaPriya-02 <140167236+SathyaPriya-02@users.noreply.github.com>
2023-08-08 22:36:56 +0530

    Update MainClass.java

--- Sathyapriya J/Test/MainClass.java
+++ Sathyapriya J/Test/MainClass.java
@@ -13,6 +13,12 @@ class MainClass
 		s.add(Song.createSong("AAA,Pop,04:35,4.5"));
 		s.add(Song.createSong("BBB,Melody,03:20,3.5"));
 		s.add(Song.createSong("CCC,Rock,05:00,4.7"));
+		s.add(Song.createSong("DDD,Pop,02:35,3.0"));
+		s.add(Song.createSong("EEE,Melody,04:20,3.5"));
+		s.add(Song.createSong("FFF,Rock,05:00,4.7"));
+		s.add(Song.createSong("GGG,Pop,05:35,4.2"));
+		s.add(Song.createSong("HHH,Melody,04:30,3.5"));
+		s.add(Song.createSong("III,Rock,05:00,3.7"));
 		
 		
 		Scanner sc = new Scanner(System.in);
@@ -35,7 +41,7 @@ class MainClass
  					s.sort((s1,s2) -> s1.getDuration().compareTo(s2.getDuration()));
 					for(Song i :s)
 					{
-						System.out.println(i);
+						System.out.format("%-20s %-10s %-12s %-12s\n",i.getTitle(),i.getGenre(),new SimpleDateFormat("mm:ss").format(i.getDuration()),i.getRating());
 						
 					}
 					break;	
@@ -45,7 +51,7 @@ class MainClass
 					s.sort((s1,s2) -> s1.getRating().compareTo(s2.getRating()));
 					for(Song i :s)
 					{
-						System.out.println(i);
+						System.out.format("%-20s %-10s %-12s %-12s\n",i.getTitle(),i.getGenre(),new SimpleDateFormat("mm:ss").format(i.getDuration()),i.getRating());
 						
 					}
 					break;	
