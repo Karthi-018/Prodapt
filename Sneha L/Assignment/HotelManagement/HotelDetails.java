185f4da9d0c6b2df517c7b6537b5655618acd671
SNEHA L <84729040+Sneha-15-2001@users.noreply.github.com>
2023-08-09 00:09:23 +0530

    Create HotelDetails.java

---
+++ Sneha L/Assignment/HotelManagement/HotelDetails.java
@@ -0,0 +1,67 @@
+import  java.io.*;
+public class HotelDetails implements Serializable{
+    private int hotelId;
+    private  String hotelName;
+    private String hotelDescription;
+    private String hotelFacilities;
+    private double hotelRating;
+
+    public HotelDetails(int hotelId, String hotelName, String hotelDescription, String hotelFacilities, double hotelRating) {
+        this.hotelId = hotelId;
+        this.hotelName = hotelName;
+        this.hotelDescription = hotelDescription;
+        this.hotelFacilities = hotelFacilities;
+        this.hotelRating = hotelRating;
+    }
+
+    public int getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(int hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getHotelDescription() {
+        return hotelDescription;
+    }
+
+    public void setHotelDescription(String hotelDescription) {
+        this.hotelDescription = hotelDescription;
+    }
+
+    public String getHotelFacilities() {
+        return hotelFacilities;
+    }
+
+    public void setHotelFacilities(String hotelFacilities) {
+        this.hotelFacilities = hotelFacilities;
+    }
+
+    public double getHotelRating() {
+        return hotelRating;
+    }
+
+    public void setHotelRating(double hotelRating) {
+        this.hotelRating = hotelRating;
+    }
+
+    @Override
+    public String toString() {
+        return "HotelDetails{" +
+                "hotelId=" + hotelId +
+                ", hotelName='" + hotelName + '\'' +
+                ", hotelDescription='" + hotelDescription + '\'' +
+                ", hotelFacilities='" + hotelFacilities + '\'' +
+                ", hotelRating=" + hotelRating +
+                '}';
+    }
+}
