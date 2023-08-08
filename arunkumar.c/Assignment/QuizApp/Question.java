bdfa512df4cdc0b5309a0ad1db16fd78f98626bb
4RUN-30 <102476267+4RUN-30@users.noreply.github.com>
2023-08-08 09:34:57 +0530

    Update Question.java

--- arunkumar.c/Assignment/QuizApp/Question.java
+++ arunkumar.c/Assignment/QuizApp/Question.java
@@ -2,18 +2,54 @@ package mcq;
 public class Question {
     private String question;
     private String answer;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String userGuess;
 
-    public Question(String question, String answer) {
+    public String getOption1() {
+        return option1;
+    }
+
+    public String getOption2() {
+        return option2;
+    }
+
+
+    public String getUserGuess() {
+        return userGuess;
+    }
+
+    public void setUserGuess(String userGuess) {
+        this.userGuess = userGuess;
+    }
+
+    public Question(String question, String option1, String option2, String option3, String answer) {
         this.question = question;
         this.answer = answer;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
     }
 
     @Override
     public String toString() {
-        return "Question{" +
-                "question='" + question + '\'' +
-                ", answer='" + answer + '\'' +
-                '}';
+        return "Question\n" + question + "\n"+
+                "A)" + option1 + "\n" +
+                "B)" + option2 + "\n" +
+                "C)" + option3 + "\n";
+    }
+
+    public void setOption1(String option1) {
+        this.option1 = option1;
+    }
+
+    public void setOption2(String option2) {
+        this.option2 = option2;
+    }
+
+    public void setOption3(String option3) {
+        this.option3 = option3;
     }
 
     public String getQuestion() {
