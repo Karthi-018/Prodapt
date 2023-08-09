4d1eb0839cc0492b191cdb898370fbdd60db7346
maya-06 <140168019+maya-06@users.noreply.github.com>
2023-08-09 23:15:30 +0530

    Delete Maya/Hotel directory

--- Maya/Hotel/HotelNotFoundException.java
+++
@@ -1,5 +0,0 @@
-public class HotelNotFoundException extends Exception {
-    public HotelNotFoundException(String hotelName) {
-        System.out.println("Hotel not found: " + hotelName);
-    }
-}
