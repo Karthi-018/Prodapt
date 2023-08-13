9d8ba730216c576b5f8fd376450a907871a4969c
jerish304 <101175958+jerish304@users.noreply.github.com>
2023-08-13 23:19:23 +0530

    Add files via upload

---
+++ Jerish Johnson/Assingments/Employee CRUD using DB/Eservice.java
@@ -0,0 +1,103 @@
+package employee.pojo;
+
+import java.util.*;
+import java.sql.*;
+
+class Eservice {
+	Scanner sc=new Scanner(System.in);
+	PreparedStatement ps;
+	ResultSet rs;
+	Employee e=new Employee();
+	
+	public Employee searchid(Connection con,int  sid) throws Exception{
+		
+		Employee emp=null;
+		ps=con.prepareStatement("select * from new_table where eid=?");
+		ps.setInt(1, sid);
+		rs=ps.executeQuery();
+		while(rs.next()) {
+			emp=new Employee(rs.getInt(1),rs.getString(2),rs.getInt(3));
+		}
+		return emp;	
+	}
+	
+	public void createdet(Connection con)throws Exception {
+		System.out.println("Enter the id : ");
+		int id=sc.nextInt();
+
+		if (searchid(con,id)==null) {
+			System.out.print("Enter the name :");
+			String name=sc.next();
+			System.out.print("Enter the salary :");
+			int salary=sc.nextInt();
+			ps=con.prepareStatement("insert into new_table values(?,?,?)");
+			ps.setInt(1,id);
+			ps.setString(2,name);
+			ps.setInt(3,salary);
+			int result=ps.executeUpdate();
+			System.out.println("New employee created");	
+		}
+		else {
+			System.out.println("Employee details already present ");
+		}
+	}
+	public void update(Connection con) throws Exception {
+		System.out.print("Enter the id : ");
+		int uid=sc.nextInt();
+		if (searchid(con,uid)==null) {
+			System.out.println("Employee details not present");
+		}
+		else {
+			System.out.println("1.To update the name ");
+			System.out.println("2.To update the salary");
+			System.out.print("Enter the choice what to update :");
+			e.setEid(sc.nextInt());
+			switch(e.getEid()) {
+			case 1:
+				System.out.print("Enter the updated name : ");
+				e.setEname(sc.next());
+				ps=con.prepareStatement("update new_table set ename=? where eid=?");
+				ps.setString(1, e.getEname());
+				ps.setInt(2, uid);
+				ps.executeUpdate();
+				break;
+			case 2:
+				System.out.print("Enter the updated salary : ");
+				e.setSalary(sc.nextInt());
+				ps=con.prepareStatement("update new_table set salary=? where eid=?");
+				ps.setInt(1, e.getSalary());
+				ps.setInt(2, e.getEid());
+				ps.executeUpdate();
+				break;
+			}
+			System.out.println("Employee updated successfully");
+		}
+	}
+	public void display(Connection con) throws SQLException {
+		ps=con.prepareStatement("select * from new_table");
+		rs =ps.executeQuery();
+		while(rs.next()) {
+			e.setEid(rs.getInt(1));
+			e.setEname(rs.getString(2));
+			e.setSalary(rs.getInt(3));
+			System.out.println(e);
+		}
+		
+	}
+	public void delete(Connection con ) throws Exception {
+		System.out.print("Enter the employee id to delete : ");
+		e.setEid(sc.nextInt());
+		
+		if (searchid(con,e.getEid())==null) {
+			System.out.println("Employee detail not found");
+		}
+		else {
+			ps=con.prepareStatement("delete from new_table where eid=?");
+			ps.setInt(1,e.getEid());
+			ps.executeUpdate();
+			System.out.println("Deleted Successfully");
+		}
+	}
+
+	
+}
