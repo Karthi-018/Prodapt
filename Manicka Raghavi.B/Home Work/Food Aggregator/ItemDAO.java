d0b4d914c3cd41742dca152f55e940926453beec
Ragz01 <72142578+Ragz01@users.noreply.github.com>
2023-08-14 21:16:29 +0530

    Update ItemDAO.java

--- Manicka Raghavi.B/Home Work/Food Aggregator/ItemDAO.java
+++ Manicka Raghavi.B/Home Work/Food Aggregator/ItemDAO.java
@@ -38,14 +38,14 @@ public class ItemDAO
 	   ArrayList<Item>typeList=new ArrayList<>();
 	   
 	   Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/itemslist","root","root"); 
-       ps=con.prepareStatement("select*from item where type=?");
-       ps.setString(1,type);
+           ps=con.prepareStatement("select*from item where type=?");
+           ps.setString(1,type);
 	   rs= ps.executeQuery();   
 	   while(rs.next())
 	   {
 		   typeList.add(new Item(rs.getString(1),rs.getDouble(2),rs.getString(3))); 
 	   }
-       return typeList;
+           return typeList;
    }
  
    //Method to find items based on price 
@@ -54,14 +54,14 @@ public class ItemDAO
 	   ArrayList<Item>priceList=new ArrayList<>();
 	 
 	   Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/itemslist","root","root"); 
-       ps=con.prepareStatement("select*from item where price=?");
-       ps.setDouble(1,price);
+           ps=con.prepareStatement("select*from item where price=?");
+           ps.setDouble(1,price);
 	   rs= ps.executeQuery();  
 	   while(rs.next())
 	   {
 		   priceList.add(new Item(rs.getString(1),rs.getDouble(2),rs.getString(3)));
 	   }
-       return priceList;
+           return priceList;
    }
  
    //Main Method
@@ -80,63 +80,63 @@ public class ItemDAO
 		   switch(choice)
 		   {
 		     case 1:
-			        System.out.println("OPTION 1-ADDING AN ITEM\n");
-			        System.out.println("Enter the item to be added in the database-name,price,type");  
-		            obj.addItem(Item.itemObject(sc.next())); 
-		            System.out.println("Item added successfully\n");  
-			        break;
+				   System.out.println("OPTION 1-ADDING AN ITEM\n");
+			           System.out.println("Enter the item to be added in the database-name,price,type");  
+		                   obj.addItem(Item.itemObject(sc.next())); 
+		                   System.out.println("Item added successfully\n");  
+			           break;
 			   
 		     case 2:
-			        System.out.println("OPTION 2-ADDING BULK COPY OF ITEMS\n");
-		            System.out.println("Enter the number of items to be added");
-		            int number=sc.nextInt();
-		            ArrayList<Item> itemList=new ArrayList<>();
-		            for(int i=0;i<number;i++)
-		            {
-		            	System.out.println("Enter the item to be added in the database-name,price,type");  
-		    	        itemList.add(Item.itemObject(sc.next()));
-		            }
-		            obj.bulkCopy(itemList);
-			        break;
+				   System.out.println("OPTION 2-ADDING BULK COPY OF ITEMS\n");
+		                   System.out.println("Enter the number of items to be added");
+		                   int number=sc.nextInt();
+		                   ArrayList<Item> itemList=new ArrayList<>();
+		                   for(int i=0;i<number;i++) 
+				   {
+					   System.out.println("Enter the item to be added in the database-name,price,type");  
+		    	                   itemList.add(Item.itemObject(sc.next()));
+		                   }
+		                   obj.bulkCopy(itemList);
+			           break;
 			   
 		     case 3:
-			        System.out.println("OPTIONS 3- FINDING THE ITEMS BASED ON TYPE\n"); 
-			        System.out.print("Enter the type:");
-			        List<Item> typeList=obj.findItem(sc.next()); 
-			        if(typeList.size()==0)
-			        	System.out.println("No such item is present");
-			        else
-			        {
-			        	System.out.format("%-20s %5s %s\n","Name","Price","Type");
-				        for(Item item:typeList)
-				        {
-				        	System.out.format("%-20s %5s %s\n",item.getName(),item.getPrice(),item.getType());
-				        }
-			        }
+				   System.out.println("OPTIONS 3- FINDING THE ITEMS BASED ON TYPE\n"); 
+			           System.out.print("Enter the type:");
+			           List<Item> typeList=obj.findItem(sc.next()); 
+			           if(typeList.size()==0)
+					   System.out.println("No such item is present");
+			           else
+			           {
+					   System.out.format("%-20s %5s %s\n","Name","Price","Type");
+				           for(Item item:typeList)
+					   {
+						   System.out.format("%-20s %5s %s\n",item.getName(),item.getPrice(),item.getType());
+				           }
+			           }
 			        break;
 			  
 		     case 4:
-			        System.out.println("OPTIONS 4- FINDING THE ITEMS BASED ON PRICE\n"); 
-			        System.out.println("Enter the price:");
-			        List<Item> priceList=obj.findItem(sc.nextDouble()); 
-			        if(priceList.size()==0)
-			        	System.out.println("No such item is present");
-			        else
-			        {   
-			        	System.out.format("%-20s %5s %s\n","Name","Price","Type"); 
-				        for(Item item:priceList)
-				        {
-				        	System.out.format("%-20s %.1f %s\n",item.getName(),item.getPrice(),item.getType());
-				        }
-			        }
-			        break;
+				   System.out.println("OPTIONS 4- FINDING THE ITEMS BASED ON PRICE\n"); 
+			           System.out.println("Enter the price:");
+			           List<Item> priceList=obj.findItem(sc.nextDouble()); 
+			           if(priceList.size()==0)
+					   System.out.println("No such item is present");
+			           else
+			           {   
+					   System.out.format("%-20s %5s %s\n","Name","Price","Type"); 
+				           for(Item item:priceList)
+					   {
+						   System.out.format("%-20s %.1f %s\n",item.getName(),item.getPrice(),item.getType());
+				           }
+			           }
+			           break;
 			   
 		     default:
-			         System.out.println("Invalid Choice");
+				   System.out.println("Invalid Choice");
 			    
 		}
 		System.out.println("WISH TO CONTINUE ->1 EXIT->0");
 		flag=sc.nextInt();
-	}
-  }
+	 }
+    }
 }
