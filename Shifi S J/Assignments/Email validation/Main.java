8c2bc0bf385228cd20bd1376eff346fe76b243ad
shifisj21 <140167033+shifisj21@users.noreply.github.com>
2023-08-14 17:58:54 +0530

    Add files via upload

---
+++ Shifi S J/Assignments/Email validation/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String email = sc.next();
+
+        boolean email_id = validateEmail(email);
+
+
+        if (email_id) {
+
+            System.out.println("Email is valid");
+
+        } else {
+
+            System.out.println("Email is invalid");
+
+        }
+
+    }
+
+
+    static boolean validateEmail(String email) {
+
+        if (email.matches("^[a-z]([a-z0-9_.])+@[a-z]+.[a-z]{2,6}$")) {
+
+            return true;
+
+        } else {
+
+            return false;
+
+        }
+
+    }
+
+}
+
+
+
+
