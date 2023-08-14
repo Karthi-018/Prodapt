ccc66110dba35b977837279679c76417e5db2ebb
Varsha-0108 <140167235+Varsha-0108@users.noreply.github.com>
2023-08-14 23:34:13 +0530

    Add files via upload

---
+++ Varsha S/Assignments/Email validation/Email.java
@@ -0,0 +1,20 @@
+public class Email
+{
+    private String mail;
+
+    public Email(String mail)
+    {
+        this.mail = mail;
+    }
+
+    public String getMail()
+    {
+        return mail;
+    }
+
+    public void setMail(String mail)
+    {
+        this.mail = mail;
+    }
+
+}
