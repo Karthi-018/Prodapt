ecfaa1c06e6651ca1e2896feed7a502d56972a7e
Bhargavi-g2001 <139844117+Bhargavi-g2001@users.noreply.github.com>
2023-08-15 09:51:28 +0530

    Add files via upload

---
+++ BHARGAVI G/Assignments/Email validation/Main.java
@@ -0,0 +1,33 @@
+
+
+import java.util.*;
+import java.util.regex.*;
+public class Main{
+   static boolean flag = false;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("**************THIS IS EMAIL VALIDATION****************");
+        System.out.println("Enter Email id to validate: ");
+        String email = sc.next();
+
+        boolean e=validateEmail(email);
+
+        if (e) {
+            System.out.println(email + " is valid ");
+        } else {
+            System.out.println(email + " is invalid");
+        }
+
+    }
+
+    public static boolean validateEmail(String email)
+    {
+        String exp = "[a-z][a-zA-Z0-9._]+[@][a-z]+[.][a-z]{2,6}";
+        Pattern pa = Pattern.compile(exp);
+        Matcher mat = pa.matcher(email);
+        flag = mat.matches();
+        return flag;
+    }
+
+}
