e0d8d6e6abbc0488482d93defcbc2719aebd9bed
RANJANAA20 <84500585+RANJANAA20@users.noreply.github.com>
2023-08-17 19:59:28 +0530

    Create mainUniversity.java

---
+++ Assessment/Ranjanaa /mainUniversity.java
@@ -0,0 +1,91 @@
+package university;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class mainUniversity 
+{
+
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		CollegeBO bo = new CollegeBO();
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Loaded succesfully");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/prodapt","root","root");
+			System.out.println("Connection succesfull");
+
+			List<College> l = new ArrayList<>();
+			int choice =0;
+			while(choice <3)
+			{
+				System.out.println("Enter 1 to add list of college: ");
+				System.out.println("Enter 2 to search by college name: ");
+				System.out.println("Enter 3 to search by date: ");
+				System.out.println("Enter 4 to search by location: ");
+				choice = sc.nextInt();
+				switch(choice)
+				{
+				case 1 : 
+					System.out.println("Enter no.of.colleges to add: ");
+					int no = sc.nextInt();
+					for(int i=0;i<no;i++)
+					{
+					System.out.println("Enter college details to add into a table: ");
+					String input = sc.next();
+					String[] inputsplit = input.split(",");
+					String name = inputsplit[0];
+					String website = inputsplit[1];
+					String mobile = inputsplit[2];
+					String founder = inputsplit[3];
+					int numberOfDept = Integer.parseInt(inputsplit[4]);
+					String location = inputsplit[5];
+					DateFormat df=new SimpleDateFormat("DD-MM-YYYY");
+					java.util.Date utilDate=  df.parse(inputsplit[6]);
+					java.sql.Date startingDate = new java.sql.Date(utilDate.getTime());
+				    // adding input to pojo
+					College c = new College(name, website,mobile,founder,numberOfDept,location,startingDate) ;
+					l.add(c);
+					}
+					bo.bulk(con,l);
+				System.out.println("Added");
+				break;
+				
+				case 2 : 
+					System.out.println("Enter college name to search: ");
+					String uname = sc.next();
+					List<College> l1= bo.findCollege(uname,con);
+			
+					break;
+					
+				case 3 : 
+					System.out.println("Enter college name to search: ");
+					String ulocation = sc.next();
+					List<College> l2= bo.findLocation(ulocation,con);
+					break;
+					
+				case 4 :
+					System.out.println("Enter date to search: ");
+					String udate = sc.next();
+					DateFormat df = new SimpleDateFormat("DD-MM-YYYY");
+					List<College> l3 = bo.findDate(df.format(udate),con);
+					break;
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+
+}
