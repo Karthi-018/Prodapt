bbe60acab7d297fe4293ea779eb5d8a72a03ac37
GayathriR80376 <140167544+GayathriR80376@users.noreply.github.com>
2023-08-17 18:40:43 +0530

    Update maincollege.java

--- Assessment/Gayathri. R/maincollege.java
+++ Assessment/Gayathri. R/maincollege.java
@@ -1,17 +1,40 @@
 package edu.prodapt.jdbc;
 import java.util.*;
+import java.sql.*;
+import java.sql.Date;
+import java.text.*;
 public class maincollege {
 	
 	public static void main(String a[]) throws Exception
 	{
+		try
+		{
 		ArrayList<college> al=new ArrayList<>();
 		college o=new college();
 		Scanner s=new Scanner(System.in);
 		System.out.println("enter the line");
 		String line=s.next();
 		al.add(o.adddata(line));
-		
-		
+		System.out.println(al);
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/sys","root","root");
+		PreparedStatement p=c.prepareStatement("insert  college values(?,?,?,?,?,?,?)");
+		for(college ob:al)
+		{
+			p.setInt(1, ob.getNumberOfDept());
+			p.setString(2, ob.getName());
+			p.setString(3,ob.getLocation());
+			p.setString(4, ob.getFounder());
+			p.setString(5, ob.getMobile());
+			p.setString(6, ob.getWebsite());
+			p.setObject(7,(Date)ob.getStartingDate());
+			p.executeUpdate();		}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
 	}
+	
 
 }
