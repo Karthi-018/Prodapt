cbb95a0ed9cc914feca06086602b52f96fc59f81
adhilakshmiragupathi <139843963+adhilakshmiragupathi@users.noreply.github.com>
2023-08-17 17:52:44 +0530

    Add files via upload

---
+++ Assessment/Adhilakshmi/CollegeBO.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.*;
+
+public class CollegeBO {
+    publicList<College>findCollege(List<String>nameList){
+
+    }
+    publicList<College>findCollege(Date startingDate){
+
+    }
+    publicList<College>findCollege(String location){
+
+    }
+}
