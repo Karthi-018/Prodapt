319be6c5154be611590043162f2e7b7f31550498
MohammedFAZIL-H <140168392+MohammedFAZIL-H@users.noreply.github.com>
2023-08-17 19:27:50 +0530

    CollegeBO.java

--- Assessment/Mohammed Fazil H/test4/CollegeBO.java
+++ Assessment/Mohammed Fazil H/test4/CollegeBO.java
@@ -1,58 +1,55 @@
 package test4;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CollegeBO {
-	public List<College> findCollege(List<String> nameList) {
-		  
-		  if (nameList.isEmpty()) {
-		    return new ArrayList<>();
-		  }
-		  
-		  for (String sname : nameList) {
-		    
-		    College college = college.findByName(nameList);
-		    
-		    if (college != null) {
-		      college.add(college);
-		    }
-		  }
-		  
-		  return college;
+	DbConnection db = new DbConnection();
+	Connection con = db.createConnection();
+	PreparedStatement ps;
+	ResultSet rs;
+	
+	List<College> find = new ArrayList<>();
+	public List<College> findCollege(List<String> nameList) throws SQLException{
+		
+		for(int i=0;i<nameList.size();i++) {
+			ps = con.prepareStatement("select * from college where name = ?");
+			ps.setString(1,nameList.get(i));
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				find.add(new College(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getString(6),rs.getDate(7)));
+			}
 		}
-
-	public List<College> findCollege(String location) {
-		 
-		  if (!isValidLocation(location)) {
-		    return null;
-		  }
-		  
-		  List<College> colleges = getColleges();
-		 
-		  List<College> foundColleges = new ArrayList<>();
-		  for (College col: colleges) {
-		    if (col.getLocation().equals(location)) {
-		      foundColleges.add(col);
-		    }
-		  }
-		  
-		  return foundColleges;
-		}
-
-	private List<College> getColleges() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	private boolean isValidLocation(String location) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	public void add(List<College> list) {
-		// TODO Auto-generated method stub
+		return find;
 		
 	}
-
+	
+	public List<College> findCollege(Date startingDate) throws SQLException{
+		find  = null;
+		ps = con.prepareStatement("select * from college where date = ?");
+		ps.setDate(1, startingDate);
+		rs = ps.executeQuery();
+		while(rs.next()) {
+			find.add(new College(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getString(6),rs.getDate(7)));
+		}
+		return find;
+		
+	}
+	
+	public List<College> findCollege(String location) throws SQLException{
+		find = null;
+		ps = con.prepareStatement("select * from college where location = ?");
+		ps.setString(1, location);
+		rs = ps.executeQuery();
+		while(rs.next()) {
+			find.add(new College(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getString(6),rs.getDate(7)));
+		}
+		return find;
+		
+	}
 }
