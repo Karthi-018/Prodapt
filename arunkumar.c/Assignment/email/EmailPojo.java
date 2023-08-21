65b1c72329cdeb20f526864a609055e15a17976e
4RUN-30 <102476267+4RUN-30@users.noreply.github.com>
2023-08-21 10:02:47 +0530

    Create EmailPojo.java

---
+++ arunkumar.c/Assignment/email/EmailPojo.java
@@ -0,0 +1,21 @@
+public class EmailPojo {
+    private String email;
+
+    public EmailPojo(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Email=" + email ;
+    }
+}
