9490b83695c6ac156205ad1dc3491d0f5db0515f
SNEHA L <84729040+Sneha-15-2001@users.noreply.github.com>
2023-08-26 18:56:15 +0530

    Add files via upload

---
+++ Sneha L/Class Work/Servlet/HttpSession1/LogoutServlet.java
@@ -0,0 +1,33 @@
+package edu.Session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException,ServletException {
+		HttpSession session=req.getSession(false);
+		if(session!=null)
+		{
+			session.invalidate();
+		}
+		PrintWriter out=res.getWriter();
+		out.println("<html>\r\n"
+				+ "	<head>\r\n"
+				+ "		<meta charset=\"UTF-8\">\r\n"
+				+ "<title>Logout</title>\r\n"
+				+ "	</head>\r\n"
+				+ "	<body>\r\n"
+				+ "		<h2>Logout Successful</h2>\r\n"
+				+ "		<p>You have been SUCCESSFULLY LOGGED OUT</p>\r\n"
+				+ "	</body>\r\n"
+				+ "</html>");
+	}
+
+}
