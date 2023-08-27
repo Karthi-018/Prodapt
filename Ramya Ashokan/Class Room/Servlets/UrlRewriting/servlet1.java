b1d0f6af9cab033b246305b302c9e6f11c0e2884
Ramya-Ashokan <140167902+Ramya-Ashokan@users.noreply.github.com>
2023-08-28 00:54:13 +0530

    Add files via upload

---
+++ Ramya Ashokan/Class Room/Servlets/UrlRewriting/servlet1.java
@@ -0,0 +1,50 @@
+package urlrewriting;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servlet1
+ */
+@WebServlet("/servlet1")
+public class servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    String uname=request.getParameter("myname");
+	    request.setAttribute("uk",uname);
+		response.sendRedirect("Servlet2?name="+request.getParameter("myname"));
+	    //RequestDispatcher rd=request.getRequestDispatcher("/Servlet2");
+		//rd.forward(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
