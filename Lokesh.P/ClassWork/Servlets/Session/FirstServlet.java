4e0a4469fd1beb82c519cfd4d279e46d119e0652
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2023-08-27 11:58:39 +0530

    Add files via upload

---
+++ Lokesh.P/ClassWork/Servlets/Session/FirstServlet.java
@@ -0,0 +1,21 @@
+package com.session;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class FirstServlet extends HttpServlet {
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException, IOException {
+		HttpSession session=req.getSession();
+		res.setContentType("text/html");
+		session.setAttribute("username",req.getParameter("uname"));
+		RequestDispatcher rd=req.getRequestDispatcher("/secondserv");
+		rd.forward(req, res);
+	}
+
+}
---
+++ Lokesh.P/ClassWork/Servlets/Session/SecondServlet.java
@@ -0,0 +1,21 @@
+package com.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SecondServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req,HttpServletResponse res) throws IOException {
+		HttpSession session=req.getSession();
+		String username=(String)session.getAttribute("username");
+		res.setContentType("text/html");
+		PrintWriter out=res.getWriter();
+		out.println("Username retrieved from session:"+username);
+				
+	}
+}
