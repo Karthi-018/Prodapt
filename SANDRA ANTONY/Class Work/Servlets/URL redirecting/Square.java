1e33674a3e9c07edb0b08d71e847b79ab89e35f4
SandraAnto02 <140167149+SandraAnto02@users.noreply.github.com>
2023-08-28 22:12:52 +0530

    Update Square.java

--- SANDRA ANTONY/Class Work/Servlets/URL redirecting/Square.java
+++ SANDRA ANTONY/Class Work/Servlets/URL redirecting/Square.java
@@ -10,7 +10,7 @@ public class Square extends HttpServlet
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
 	{
 		
-		int k =(int)req.getAttribute("k");			//type casting and then getting the value from addServ to Square
+		int k =Integer.parseInt(req.getAttribute("k"));			//type casting and then getting the value from addServ to Square
 		
 		k=k*k;
 		
