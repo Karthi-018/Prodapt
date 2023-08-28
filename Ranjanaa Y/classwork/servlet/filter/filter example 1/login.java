914f4fddf5a65777c1b5cea13b45d94c3683dbee
RANJANAA20 <84500585+RANJANAA20@users.noreply.github.com>
2023-08-28 22:51:46 +0530

    Create login.java

---
+++ Ranjanaa Y/classwork/servlet/filter/filter example 1/login.java
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action = "Servlet" method = "post">
+	USERNAME<input type = "text" name = "uname" value = ""><br>
+	PASSWORD<input type = "text" name = "pwd" value = ""><br>
+	<input type = "submit" value="ok">
+</form>
+</body>
+</html>
