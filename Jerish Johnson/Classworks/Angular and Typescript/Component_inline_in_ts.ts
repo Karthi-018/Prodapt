dbfd7a4a23581059a2db2366c831435d06de1c44
jerish304 <101175958+jerish304@users.noreply.github.com>
2023-09-08 16:20:50 +0530

    Create Component_inline_in_ts.ts

---
+++ Jerish Johnson/Classworks/Angular and Typescript/Component_inline_in_ts.ts
@@ -0,0 +1,7 @@
+@Component({
+  selector: 'app-root',
+  template :`<h1>Hi Hello Welcome to Prodapt</h1>`,
+  styles: ['h1{color:red}']
+  //  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+})
