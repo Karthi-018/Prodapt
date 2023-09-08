4b7c45b55dd80581a4bdee6bb210357962889987
shifisj21 <140167033+shifisj21@users.noreply.github.com>
2023-09-08 17:39:33 +0530

    Add files via upload

---
+++ Shifi S J/Classwork/Angular/demo.js
@@ -0,0 +1,24 @@
+// myFun();
+// function myFun()
+// {
+//     console.log("Hi Shifi");
+// }
+// let number=10;
+// console.log(number)   //gives error
+var num = 10;
+console.log(typeof (num));
+var sname;
+sname = '35';
+console.log(typeof (sname));
+var markss;
+console.log(typeof (markss));
+var marksss = 100;
+console.log(typeof (marksss));
+// num=true;
+// num="JS"
+var marks;
+marks = "ABC";
+console.log(typeof (marks));
+var i = 10;
+i = 'js';
+i = true;
---
+++ Shifi S J/Classwork/Angular/demo.ts
@@ -0,0 +1,32 @@
+// myFun();
+// function myFun()
+// {
+//     console.log("Hi Shifi");
+
+// }
+
+
+// let number=10;
+// console.log(number)   //gives error
+
+let num=10;
+console.log(typeof(num))
+var sname;
+sname='35';
+console.log(typeof(sname));
+var markss;
+console.log(typeof(markss))
+let marksss=100;
+console.log(typeof(marksss))
+
+// num=true;
+// num="JS"
+
+let marks;
+marks="ABC";
+console.log(typeof(marks))
+
+let i:any=10;
+console.log(i),
+i='js'
+i=true
