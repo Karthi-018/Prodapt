30e1e626fe2b118dcdd980ced35a3b731cfe53f4
saisudha-g <140166967+saisudha-g@users.noreply.github.com>
2023-09-28 21:50:27 +0530

    Add files via upload

--- Saisudha G/assignment/Angularecom/src/app/product-module/search-product/search-product.component.ts
+++ Saisudha G/assignment/Angularecom/src/app/product-module/search-product/search-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductServiceService } from 'src/app/product-service.service';
 
 @Component({
   selector: 'app-search-product',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-product.component.css']
 })
 export class SearchProductComponent {
+searchlist: any;
+ 
+  find: any='';
+constructor(private eservice:ProductServiceService){
 
+}
+viewopr(){
+  this.searchlist=this.eservice.searchproduct(this.find).subscribe((e: any)=>{if(Array.isArray(e))
+  {
+    console.log(e);
+    this.searchlist=e;
+    console.log(this.searchlist);
+  }})
+}
 }
